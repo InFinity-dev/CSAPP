97d5321105e98051705b93947578eaca1c465233
이민섭 <31795815+InFinity-dev@users.noreply.github.com>
2022-12-12 17:07:42 +0900

    echo refactoring

--- webservers/echo/echoserveri.c
+++ webservers/echo/echoserveri.c
@@ -17,14 +17,28 @@ int main(int argc, char **argv){
         exit(0);
     }
 
-    listenfd = Open_clientfd(argv[1]);
+    listenfd = Open_listenfd(argv[1]);
     while(1){
         clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //포인터 타입 캐스팅
         Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port,
                     MAXLINE, 0);
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
         echo(connfd);
         Close(connfd);
     }
     exit(0);
+}
+//텍스트 줄을 읽고 echo해주는 echo 함수
+void echo(int connfd){
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
 }
