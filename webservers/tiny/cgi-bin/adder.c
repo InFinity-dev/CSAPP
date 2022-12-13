5bcf13ac3e7f831ba6f25b3bff1e4faccc8a390d
이민섭 <31795815+InFinity-dev@users.noreply.github.com>
2022-12-13 23:33:24 +0900

    adder implemented to tiny

--- webservers/tiny/cgi-bin/adder.c
+++ webservers/tiny/cgi-bin/adder.c
@@ -12,11 +12,26 @@ int main (void){
     if ((buf = getenv("QUERY_STRING")) !=NULL){
         p = strchr(buf,'&');
         *p = '\0';
-//http://127.0.0.1:8000/cgi-bin/adder?1&2
+        /* 책에 나온 코드는 테스트 용으로 GET 방식으로 넘겨줄때 변수에 대한 이름을 지정해 주지 않았지만
+         * home.html에 form 을 통한 인자 전달시 name항목이 들어가게 되므로 코드 변경이 필요하다
+         * http://127.0.0.1:8000/cgi-bin/adder?1&2*/
+
 //        strcpy(arg1, buf);
 //        strcpy(arg2, p+1);
 //        n1 = atoi(arg1); //atoi 함수 : 문자열로 받아온 정수를 int형으로 변환(문자의 경우 0 리턴)
 //        n2 = atoi(arg2);
+
+        /* form 내부에 arg1, arg2 name으로 변수를 입력받아 GET Method로 /cgi-bin/adder 로 넘겨준다
+         * 전달되는 URL형태는 http://127.0.0.1:8000/cgi-bin/adder?arg1=1&arg2=2 와 같은 형태
+         * sscanf 통해 각각 n1, n2로 받아올수 있다.
+         * <form action="/cgi-bin/adder" method="get" target="_blank">
+            <label>Adder argument 1:</label>
+            <input type="text" name="arg1"><br><br>
+            <label>Adder argument 1:</label>
+            <input type="text" name="arg2"><br><br>
+            <input type="submit" value="Submit">
+        </form>*/
+
         sscanf(buf, "arg1=%d", &n1);
         sscanf(p+1, "arg2=%d", &n2);
 
